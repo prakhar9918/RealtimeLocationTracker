b503a2d91fc285f9b7c6c99d38feeffbfa9fba21
prakhar9918 <itsprakhar254@gmail.com>
2025-03-08 21:28:04 +0530

    Realtime Location Tracker

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app  = express();
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+//Socketio BoilerPlate
+
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+
+io.on("connection" ,function(socket){
+    console.log("Connected to server");
+    socket.on("send-location",function(data){
+        io.emit("receive-location",{id:socket.id,...data});
+        // console.log("Received location:", data);
+    });
+    socket.on("disconnect",function(){
+       io.emit("user-disconnected" , socket.id); 
+    //    console.log("User disconnected:", id)
+    })
+});
+
+app.get("/", function(req,res){
+    res.render("index");
+});
+
+server.listen(5080);
